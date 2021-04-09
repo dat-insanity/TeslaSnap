63235b6963d38f4cd0dad14ba97d0313a5914d2d
dat-insanity <54964664+dat-insanity@users.noreply.github.com>
2021-04-10 01:17:51 +1000

    file moment

---
+++ teslasnap_daemon.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# tSnap - tesla.sexy auto uploader
+
+cd "/Users/$USER/Library/Application Support/teslasnap"
+uploadkey=$(cat uploadkey.txt | tr -d '\n')
+echo `date`\: 'Changed directory successfully to ' `pwd` >> ../logs.txt
+cd "/Users/$USER/Library/Application Support/teslasnap/temp"
+echo `date`\: 'Changed directory successfully to ' `pwd` >> ../logs.txt
+
+while :
+do
+sleep 0.2
+filecheck=$(ls -1)
+        if [[ -z $filecheck ]]
+        then
+                echo
+        else
+                echo `date`\: 'Image recieved. Uploading...'>> ../logs.txt
+                imageurl=$(curl -XPOST -H ""auth": "$uploadkey"" -F file=@"$filecheck" -F URL=$json:url$ https://api.tesla.sexy/files/upload)
+                refinedimgurl=$(echo $imageurl | tr -d '\n' | sed -e 's/^........................//' -e 's/......................................................................................................................................................................................$//')
+                echo `date`\: ''$imageurl >> ../logs.txt
+                echo $refinedimgurl | tr -d '\n' | pbcopy
+                case $refinedimgurl in
+                h*) osascript -e 'display notification "Image uploaded successfully." with title "tSnap" sound name "Submarine"' ;;
+                *) osascript -e 'display notification "An error occurred. Please ask on our discord for help." with title "tSnap" sound name "Frog"' ;;
+                        esac
+                cp -a "$filecheck" "/Users/$USER/Desktop/Screenshots/Screenshot-$(date +"%m-%d-%y-%r").png"
+                rm -f "$filecheck"
+        fi
+done
---
+++ teslasnap_installer.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# tSnap - tesla.sexy installer
+
+cd "/Users/$USER/Library/Application Support/"
+echo "[`date`] Changed directory to `pwd`"
+mkdir teslasnap
+echo "[`date`] Created teslasnap directory"
+cd "/Users/$USER/Library/Application Support/teslasnap"
+echo "[`date`] Changed directory to `pwd`"
+read -p "Paste in your tesla.sexy key: " teslauploadkey
+touch uploadkey.txt
+echo "[`date`] Created uploadkey file"
+echo $telsauploadkey tr -d '\n' > uploadkey.txt
+echo "[`date`] Wrote key to file"
+mkdir temp
+echo "[`date`] Created temp directory"
+curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/dat-insanity/TeslaSnap/main/teslasnap_daemon.sh -o teslasnap_daemon.sh
+echo "[`date`] Created and saved teslasnap_daemon"
+chmod +X "/Users/$USER/Library/Application Support/teslasnap/teslasnap_daemon.sh"
+echo "[`date`] Gave exec permissions to teslasnap_daemon"
+
+
+cd ~/Library/LaunchAgents/
+echo "[`date`] Changed directory to `pwd`"
+curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/dat-insanity/TeslaSnap/main/sexy.tesla.datinsanity.plist -o sexy.tesla.datinsanity.plist
+echo "[`date`] Created and saved sexy.tesla.datinsanity.plist"
+sed -i "s/REPLACEWITHUSERNAME/`whoami`/g" sexy.tesla.datinsanity.plist
+echo "[`date`] Created launch agent for user `whoami`"
+cd ~/Desktop
+echo "[`date`] Changed directory to `pwd`"
+mkdir Screenshots
+echo "[`date`] Created screenshots directory. All your screenshots will be saved here."
+launchctl load ~/Library/LaunchAgents/sexy.tesla.datinsanity.plist
+echo "[`date`] Load the launch agent for teslasnap, under user `whoami`"
+chown `whoami` "/Users/$USER/Application Support/teslasnap/temp"
+echo "[`date`] Give perms for temp folder"
+defaults write com.apple.screencapture location "~/Library/Application Support/teslasnap/temp"
+echo "[`date`] Route screenshots to a folder for teslasnap_daemon to upload"
+echo "[`date`] Done! Thank you for installing teslasnap! If you have issues with teslasnap, please make a ticket in our server, or dm dat_insanity#2048 \n Note: all your screenshots will now be saved in ~/Desktop/Screenshots"
